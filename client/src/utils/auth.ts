ab1b429aef25a0edcbf04b8b7363b66cc0e05cd3
Liathor <dyermisha@gmail.com>
2025-03-21 19:25:24 -0400

    Updated auth.ts in client folder

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,28 +3,50 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const decodedToken = jwtDecode<JwtPayload>(this.getToken());
+    return decodedToken;
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return token;
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const decodedToken = jwtDecode<JwtPayload>(token);
+
+      if (!decodedToken.exp) {
+        return true;
+      }
+
+       return Date.now() >= decodedToken.exp * 1000;
+
+    } catch (err) {
+      return true;
+    }
   }
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
+    localStorage.setItem('id_token', idToken);
     // TODO: redirect to the home page
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
+    localStorage.removeItem('id_token');
     // TODO: redirect to the login page
+    window.location.assign('/');
   }
 }
 
