b27dffaae6064b02d3ae0420974a244164e778ff
Liathor <dyermisha@gmail.com>
2025-03-21 18:47:41 -0400

    updated authRoutes

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,6 +5,38 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const userData = await User.findOne({ where: { username: req.body.username } });
+  const { username } = req.body;
+
+  try {
+
+  if (!userData) {
+    // the error message shouldn't specify if the login failed because of wrong email or password
+    res.status(404).json({ message: 'Login failed. Please try again!' });
+    return;
+  }
+  const validPassword = await bcrypt.compare(
+    req.body.password,
+    userData.password
+  );
+  if (!validPassword) {
+    res.status(400).json({ message: 'Login failed. Please try again!' });
+    return;
+  }
+  res.status(200).json({ message: 'You are now logged in!' });
+
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+
+    return res.json({ token });
+
+} catch (err) {
+
+    return res.status(400).json(err);
+
+}
+
 };
 
 const router = Router();
